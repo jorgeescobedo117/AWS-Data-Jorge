8d5cd4ad401c0cf824c559d6faf5cad45656d5c4
Jorge <Jorgeescobedo117@gmail.com>
2025-10-18 16:31:03 -0600

    hola

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Dashboard AWS Superstore", layout="wide")
+st.title("📊 Proyecto ETL Serverless — AWS Superstore")
+st.write("Dashboard desplegado con Docker y GitHub Actions.")
